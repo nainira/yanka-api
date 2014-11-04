8f04e078a4dc3be9b4cd898b88019ed6490e2fb3
Ryan Kim <nainirajk@gmail.com>
2014-11-04 17:43:27 +0900

    add fix for srf token authenticity issue with json request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
 
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, :with => :render_error
