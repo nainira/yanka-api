f612c60765fb9efec6884e81b0941879012ab949
Ryan Kim <nainirajk@gmail.com>
2014-11-05 09:39:54 +0900

    Revert "fix seed and nested resources"
    This reverts commit ff61ad27115b5cc19c1f19f6f17c2ea297a173c1.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,17 @@
 class CommentsController < ApplicationController
   def index
     car = Car.find(params[:car_id])
-    comments = car.comments
+    comments = car.comments.all
     render json: comments, status: 200
   end
   # def show
-  #   car = Car.find(params[:id])
-  #   comment = car.comments.all
-  #   render json: comment
+  #   car = Car.find(params[:car_id])
+  #   comment = car.comment.find[:comment_id]
   # end
 
   def create
     car = Car.find(params[:car_id])
-    comment = car.comments.build(comment_params)
+    comment = car.comments.create(comment_params)
     if comment.save
       render json: comment, status: 201
     else
